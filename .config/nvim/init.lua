ba9eb37f429dc6569bd695a1e2199a27a05ba4f0
spenserlee <Spenser.L159@gmail.com>
2023-07-12 00:09:10 -0700

    Update nvim leap config to not overwrite 'x' mapping
    Diagnosed with :verbose vmap x

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -217,6 +217,10 @@ require("lazy").setup({
         "ggandor/leap.nvim",
         config = function()
             require("leap").add_default_mappings()
+
+            -- don't overwrite the default delete keymapping, I only use sS
+            vim.keymap.del({'x', 'o'}, 'x')
+            vim.keymap.del({'x', 'o'}, 'X')
         end
     },
     {
