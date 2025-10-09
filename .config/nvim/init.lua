57e4a9c6fbd23dc3dccc17d64f1bd2271b0e2f90
spenserlee <Spenser.L159@gmail.com>
2025-10-08 17:10:36 -0700

    Fix some nvim-treesitter warnings
    Not quite sure how to fix `nvim_buf_set_option()` deprecated warning.
    Using the new API breaks folding behaviour.
    
    <https://github.com/neovim/neovim/discussions/29681>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1166,9 +1166,11 @@ require("lazy").setup({
                 -- Automatically install missing parsers when entering buffer
                 -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                 auto_install = false,
+                ignore_install = {},
+                modules = {},
                 highlight = {
                     enable = true,
-                    disable = function(lang, buf)
+                    disable = function(_, buf)
                         local max_filesize = 100 * 1024 -- 100 KB
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
