c916ac54c19cb31dc343c2b81f50eb6231989cfa
spenserlee <Spenser.L159@gmail.com>
2026-06-10 21:37:56 -0700

    Fix nvim.lsp.deprecated error
        ==============================================================================
        vim.deprecated:                                                           1 ⚠️
    
         ~
        - ⚠️ WARNING vim.lsp.get_buffers_by_client_id() is deprecated. Feature will be removed in Nvim 0.13
          - ADVICE:
            - use vim.lsp.get_client_by_id(id).attached_buffers instead.
            - stack traceback:
                /home/spenser/.local/share/nvim/lazy/rustaceanvim/lua/rustaceanvim/server_status.lua:52
                /usr/local/share/nvim/runtime/lua/vim/lsp/client.lua:1319
                /usr/local/share/nvim/runtime/lua/vim/lsp/client.lua:470
                vim/_core/editor:273

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1029,7 +1029,7 @@ require("lazy").setup({
         -- Configures rust-analyzer builtin LSP client + integrates with other Rust tools.
         -- https://github.com/mrcjkb/rustaceanvim/discussions/122
         'mrcjkb/rustaceanvim',
-        version = '^6', -- Recommended
+        version = '^9', -- Recommended
         ft = { 'rust' },
         lazy = false,
         init = function()
