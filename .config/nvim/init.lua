3fe875e8b64ff59b90713254852f9b4b3f8cfd50
Spenser Lee <Spenser.L159@gmail.com>
2026-06-11 22:12:59 -0700

    Fix :RustFmt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1131,17 +1131,10 @@ require("lazy").setup({
                 },
                 dap = {},
             }
-            -- workaround fix for error: "LSP: rust_analyzer: -32802: server cancelled the request"
-            -- https://github.com/neovim/neovim/issues/30985
-            for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
-                local default_diagnostic_handler = vim.lsp.handlers[method]
-                vim.lsp.handlers[method] = function(err, result, context, config)
-                    if err ~= nil and err.code == -32802 then
-                        return
-                    end
-                    return default_diagnostic_handler(err, result, context, config)
-                end
-            end
+
+            -- Fix :RustFmt
+            vim.g.rustfmt_emit_files = 1
+            -- vim.g.rustfmt_detect_version = 1
          end
     },
     {
